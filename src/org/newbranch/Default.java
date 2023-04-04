43fa74ad80941451d9dcf64edc33d4ff1ed35097
yugeshram <yugeshram1701@gmail.com>
2023-04-04 09:48:12 +0530

    yugeshram

---
+++ src/org/newbranch/Default.java
@@ -0,0 +1,12 @@
+package org.newbranch;
+
+public class Default {
+	private void gg() {
+		// TODO Auto-generated method stub
+       System.out.println("yugesh");
+	}
+public static void main(String[] args) {
+	Default d = new Default();
+	d.gg();
+}
+}
